5ed4664cdb441c50320080da7435c505d6985724
violining <eugenekhy@gmail.com>
2024-02-16 19:44:24 +0000

    files added

---
+++ GameOfLife.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+
+
+public class GameOfLife {
+     public static void main(String[] args) {
+          JFrame gameFrame = new JFrame();
+          gameFrame.setTitle("Game Of Life");
+          gameFrame.setSize(new Dimension(1200, 800));
+          gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          
+
+          gameFrame.setVisible(true);
+     }
+} 

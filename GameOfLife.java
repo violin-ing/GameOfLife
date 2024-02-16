1fe291811b19ef5a207ebcaa7a1cb2ee1d5a8670
violining <eugenekhy@gmail.com>
2024-02-16 23:02:33 +0000

    updated files

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,15 +1,11 @@
-import java.awt.*;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class GameOfLife {
      public static void main(String[] args) {
-          JFrame gameFrame = new JFrame();
-          gameFrame.setTitle("Game Of Life");
-          gameFrame.setSize(new Dimension(1200, 800));
-          gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
           
-
-          gameFrame.setVisible(true);
      }
-} 
+}
